be74aaf4047832cd5655a4a0b40563692827bcc3
PabloGargalloSanz <pablogargallosanz@gmail.com>
2026-01-14 10:13:00 +0100

    Feat: conectar db

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import pool from './src/db/db.js';
+
+const app= express();
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send ({
+        mensaje: "Bienvenido"
+    })
+});
+
+pool.connect()
+    .then(() => {
+        console.log('✅ Conectado a la base de datos');
+    }).catch((error) =>{
+        console.log('❌ Error al conectarse a la base de datos ', error);
+})
+
+app.listen(env.PORT, () => {
+    console.log('Servidor escuchando en el puerto ' + process.env.PORT);
+})
